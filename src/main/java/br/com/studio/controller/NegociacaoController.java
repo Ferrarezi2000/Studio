39cf9868ffd18e11c5636ede99f2094e56c0e2cd
Ferrarezi2000 <thiago.ferrarezi>
2017-03-03 13:56:49 -0300

    Negociacao Controller

---
+++ src/main/java/br/com/studio/controller/NegociacaoController.java
@@ -0,0 +1,10 @@
+package br.com.studio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/negociacao")
+public class NegociacaoController {
+
+}
